091455bd34b1d32befe2cde2318bc056b1ea1de5
jaythan <yegarrick12@gmail.com>
2024-02-01 14:39:43 +0800

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import tailwindcss from  "tailwindcss"
+import autoprefixer from "autoprefixer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {// 关键代码
+      plugins: [
+        tailwindcss, 
+        autoprefixer,
+      ]
+    },
+   },
   build: {
     lib: {
       entry: resolve(__dirname, './src/components/index.tsx'),
